a7ae8795b5026aec9108228c84a1c1e70e79ae58
Mariano Majano Amaya <mariano@bu.edu>
2024-06-01 15:30:03 -0400

    Working on signout hook

---
+++ src/hooks/use-auth.ts
@@ -0,0 +1,18 @@
+import { useRouter } from 'next/navigation'
+import { toast } from 'sonner'
+
+export const useAuth = () => {
+  const router = useRouter()
+
+  const signOut = async () => {
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/logout`,
+        {
+          method: 'POST',
+          credentials: 'include',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      )
