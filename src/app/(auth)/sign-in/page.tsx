464ca805f8dd831b4c56789130e63625a91e0f37
Mariano Majano Amaya <mariano@bu.edu>
2024-06-01 15:35:24 -0400

    Setting up the sign in form

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -88,3 +88,97 @@ const Page = () => {
               account
             </h1>
 
+            <Link
+              className={buttonVariants({
+                variant: 'link',
+                className: 'gap-1.5',
+              })}
+              href='/sign-up'>
+              Don&apos;t have an account?
+              <ArrowRight className='h-4 w-4' />
+            </Link>
+          </div>
+
+          <div className='grid gap-6'>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className='grid gap-2'>
+                <div className='grid gap-1 py-2'>
+                  <Label htmlFor='email'>Email</Label>
+                  <Input
+                    {...register('email')}
+                    className={cn({
+                      'focus-visible:ring-red-500':
+                        errors.email,
+                    })}
+                    placeholder='you@example.com'
+                  />
+                  {errors?.email && (
+                    <p className='text-sm text-red-500'>
+                      {errors.email.message}
+                    </p>
+                  )}
+                </div>
+
+                <div className='grid gap-1 py-2'>
+                  <Label htmlFor='password'>Password</Label>
+                  <Input
+                    {...register('password')}
+                    type='password'
+                    className={cn({
+                      'focus-visible:ring-red-500':
+                        errors.password,
+                    })}
+                    placeholder='Password'
+                  />
+                  {errors?.password && (
+                    <p className='text-sm text-red-500'>
+                      {errors.password.message}
+                    </p>
+                  )}
+                </div>
+
+                <Button disabled={isLoading}>
+                  {isLoading && (
+                    <Loader2 className='mr-2 h-4 w-4 animate-spin' />
+                  )}
+                  Sign in
+                </Button>
+              </div>
+            </form>
+
+            <div className='relative'>
+              <div
+                aria-hidden='true'
+                className='absolute inset-0 flex items-center'>
+                <span className='w-full border-t' />
+              </div>
+              <div className='relative flex justify-center text-xs uppercase'>
+                <span className='bg-background px-2 text-muted-foreground'>
+                  or
+                </span>
+              </div>
+            </div>
+
+            {isSeller ? (
+              <Button
+                onClick={continueAsBuyer}
+                variant='secondary'
+                disabled={isLoading}>
+                Continue as customer
+              </Button>
+            ) : (
+              <Button
+                onClick={continueAsSeller}
+                variant='secondary'
+                disabled={isLoading}>
+                Continue as seller
+              </Button>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Page
