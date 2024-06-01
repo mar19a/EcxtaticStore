79bf7225ee51c894fcd08ee30cef28e8cd5467f8
Mariano Majano Amaya <mariano@bu.edu>
2024-06-01 15:10:05 -0400

    Path to sign in and sign up is working as expected

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,3 +5,14 @@ export async function middleware(req: NextRequest) {
   const { nextUrl, cookies } = req
   const { user } = await getServerSideUser(cookies)
 
+  if (
+    user &&
+    ['/sign-in', '/sign-up'].includes(nextUrl.pathname)
+  ) {
+    return NextResponse.redirect(
+      `${process.env.NEXT_PUBLIC_SERVER_URL}/`
+    )
+  }
+
+  return NextResponse.next()
+}
