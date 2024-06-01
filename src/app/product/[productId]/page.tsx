5028eadf7922cb7c07719ee7245a76c943edd4d4
Mariano Majano Amaya <mariano@bu.edu>
2024-06-01 16:00:17 -0400

    Showcasing product details

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -38,4 +38,45 @@ const Page = async ({ params }: PageProps) => {
     },
   })
 
-  
+  const [product] = products
+
+  if (!product) return notFound()
+
+  const label = PRODUCT_CATEGORIES.find(
+    ({ value }) => value === product.category
+  )?.label
+
+  const validUrls = product.images
+    .map(({ image }) =>
+      typeof image === 'string' ? image : image.url
+    )
+    .filter(Boolean) as string[]
+
+  return (
+    <MaxWidthWrapper className='bg-white'>
+      <div className='bg-white'>
+        <div className='mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8'>
+          {/* Product Details */}
+          <div className='lg:max-w-lg lg:self-end'>
+            <ol className='flex items-center space-x-2'>
+              {BREADCRUMBS.map((breadcrumb, i) => (
+                <li key={breadcrumb.href}>
+                  <div className='flex items-center text-sm'>
+                    <Link
+                      href={breadcrumb.href}
+                      className='font-medium text-sm text-muted-foreground hover:text-gray-900'>
+                      {breadcrumb.name}
+                    </Link>
+                    {i !== BREADCRUMBS.length - 1 ? (
+                      <svg
+                        viewBox='0 0 20 20'
+                        fill='currentColor'
+                        aria-hidden='true'
+                        className='ml-2 h-5 w-5 flex-shrink-0 text-gray-300'>
+                        <path d='M5.555 17.776l8-16 .894.448-8 16-.894-.448z' />
+                      </svg>
+                    ) : null}
+                  </div>
+                </li>
+              ))}
+            </ol>
