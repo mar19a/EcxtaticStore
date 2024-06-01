d42b4fb98bc09bdf980e22917c0f29bc1899ccce
Mariano Majano Amaya <mariano@bu.edu>
2024-06-01 15:23:18 -0400

    Concealing private information in the .env file

--- src/lib/payload-utils.ts
+++ src/lib/payload-utils.ts
@@ -7,3 +7,18 @@ export const getServerSideUser = async (
 ) => {
   const token = cookies.get('payload-token')?.value
 
+  const meRes = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/me`,
+    {
+      headers: {
+        Authorization: `JWT ${token}`,
+      },
+    }
+  )
+
+  const { user } = (await meRes.json()) as {
+    user: User | null
+  }
+
+  return { user }
+}
