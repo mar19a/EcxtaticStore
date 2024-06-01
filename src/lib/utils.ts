a18290c0a8c178a09d7f139b9bfb411c2cc2bcb9
Mariano Majano Amaya <mariano@bu.edu>
2024-06-01 15:15:09 -0400

    Formatting the price and currency

---
+++ src/lib/utils.ts
@@ -0,0 +1,15 @@
+import { type ClassValue, clsx } from 'clsx'
+import { Metadata } from 'next'
+import { twMerge } from 'tailwind-merge'
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function formatPrice(
+  price: number | string,
+  options: {
+    currency?: 'USD' | 'EUR' | 'GBP' | 'BDT'
+    notation?: Intl.NumberFormatOptions['notation']
+  } = {}
+)
