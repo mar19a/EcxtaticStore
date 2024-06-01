8eed94b9f94f8da0d79affd939dc7fdd83c856dd
Mariano Majano Amaya <mariano@bu.edu>
2024-06-01 15:52:19 -0400

    Im now making the cart more professional looking, by addubg more texts

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -101,4 +101,79 @@ const Page = () => {
                   const { image } = product.images[0]
 
                   return (
+                    <li
+                      key={product.id}
+                      className='flex py-6 sm:py-10'>
+                      <div className='flex-shrink-0'>
+                        <div className='relative h-24 w-24'>
+                          {typeof image !== 'string' &&
+                          image.url ? (
+                            <Image
+                              fill
+                              src={image.url}
+                              alt='product image'
+                              className='h-full w-full rounded-md object-cover object-center sm:h-48 sm:w-48'
+                            />
+                          ) : null}
+                        </div>
+                      </div>
+
+                      <div className='ml-4 flex flex-1 flex-col justify-between sm:ml-6'>
+                        <div className='relative pr-9 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:pr-0'>
+                          <div>
+                            <div className='flex justify-between'>
+                              <h3 className='text-sm'>
+                                <Link
+                                  href={`/product/${product.id}`}
+                                  className='font-medium text-gray-700 hover:text-gray-800'>
+                                  {product.name}
+                                </Link>
+                              </h3>
+                            </div>
+
+                            <div className='mt-1 flex text-sm'>
+                              <p className='text-muted-foreground'>
+                                Category: {label}
+                              </p>
+                            </div>
+
+                            <p className='mt-1 text-sm font-medium text-gray-900'>
+                              {formatPrice(product.price)}
+                            </p>
+                          </div>
+
+                          <div className='mt-4 sm:mt-0 sm:pr-9 w-20'>
+                            <div className='absolute right-0 top-0'>
+                              <Button
+                                aria-label='remove product'
+                                onClick={() =>
+                                  removeItem(product.id)
+                                }
+                                variant='ghost'>
+                                <X
+                                  className='h-5 w-5'
+                                  aria-hidden='true'
+                                />
+                              </Button>
+                            </div>
+                          </div>
+                        </div>
+
+                        <p className='mt-4 flex space-x-2 text-sm text-gray-700'>
+                          <Check className='h-5 w-5 flex-shrink-0 text-green-500' />
+
+                          <span>
+                            Eligible for instant delivery
+                          </span>
+                        </p>
+                      </div>
+                    </li>
                   )
+                })}
+            </ul>
+          </div>
+
+          <section className='mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8'>
+            <h2 className='text-lg font-medium text-gray-900'>
+              Order summary
+            </h2>
