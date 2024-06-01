1fe80d360080b9e66b599eab94e4a215ac99dc22
Mariano Majano Amaya <mariano@bu.edu>
2024-06-01 16:02:39 -0400

    Adding a add to cart button so that the products are added to the cart

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -122,3 +122,25 @@ const Page = async ({ params }: PageProps) => {
               <ImageSlider urls={validUrls} />
             </div>
           </div>
+
+          {/* add to cart part */}
+          <div className='mt-10 lg:col-start-1 lg:row-start-2 lg:max-w-lg lg:self-start'>
+            <div>
+              <div className='mt-10'>
+                <AddToCartButton product={product} />
+              </div>
+              <div className='mt-6 text-center'>
+                <div className='group inline-flex text-sm text-medium'>
+                  <Shield
+                    aria-hidden='true'
+                    className='mr-2 h-5 w-5 flex-shrink-0 text-gray-400'
+                  />
+                  <span className='text-muted-foreground hover:text-gray-700'>
+                    30 Day Return Guarantee
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
