00d275abda41e3df3ee10eb36518a054a7adddb6
Mariano Majano Amaya <mariano@bu.edu>
2024-06-02 00:49:08 -0400

    If successful, we will send the product to the order email. If not, we will send a pending payment email and not send the product

--- src/components/PaymentStatus.tsx
+++ src/components/PaymentStatus.tsx
@@ -30,6 +30,27 @@ const PaymentStatus = ({
     if (data?.isPaid) router.refresh()
   }, [data?.isPaid, router])
 
+  return (
+    <div className='mt-16 grid grid-cols-2 gap-x-4 text-sm text-gray-600'>
+      <div>
+        <p className='font-medium text-gray-900'>
+          Shipping To
+        </p>
+        <p>{orderEmail}</p>
+      </div>
 
+      <div>
+        <p className='font-medium text-gray-900'>
+          Order Status
+        </p>
+        <p>
+          {isPaid
+            ? 'Payment successful'
+            : 'Pending payment'}
+        </p>
+      </div>
+    </div>
+  )
+}
 
 export default PaymentStatus
