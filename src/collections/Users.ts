cbfcc8c80c6bc79aa30013fd18ddf86629cbba2f
Mariano Majano Amaya <mariano@bu.edu>
2024-06-02 00:25:09 -0400

    Establishing two users type. Admin and users. If usrrs, we will generate an email using a token

---
+++ src/collections/Users.ts
@@ -0,0 +1,27 @@
+import { PrimaryActionEmailHtml } from '../components/emails/PrimaryActionEmail'
+import { Access, CollectionConfig } from 'payload/types'
+
+const adminsAndUser: Access = ({ req: { user } }) => {
+  if (user.role === 'admin') return true
+
+  return {
+    id: {
+      equals: user.id,
+    },
+  }
+}
+
+export const Users: CollectionConfig = {
+  slug: 'users',
+  auth: {
+    verify: {
+      generateEmailHTML: ({ token }) => {
+        return PrimaryActionEmailHtml({
+          actionLabel: "verify your account",
+          buttonText: "Verify Account",
+          href: `${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}`
+        })
+      },
+    },
+  },
+ 
