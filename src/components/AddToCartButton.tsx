42d61d48a600f6fdb66b14557a42b5082557a48f
Mariano Majano Amaya <mariano@bu.edu>
2024-06-02 00:29:38 -0400

    Finishing up by adding timeouts and issuccess when an item is added to cart

--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -21,5 +21,17 @@ const AddToCartButton = ({
     return () => clearTimeout(timeout)
   }, [isSuccess])
 
- 
+  return (
+    <Button
+      onClick={() => {
+        addItem(product)
+        setIsSuccess(true)
+      }}
+      size='lg'
+      className='w-full'>
+      {isSuccess ? 'Added!' : 'Add to cart'}
+    </Button>
+  )
+}
+
 export default AddToCartButton
