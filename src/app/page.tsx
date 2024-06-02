72cc27a3687053d479c7914e09a22a9a9114f04d
Mariano Majano Amaya <mariano@bu.edu>
2024-06-02 00:11:01 -0400

    Continuing setting up the layout for the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,3 +31,48 @@ const perks = [
       "Ecxtatic is more than a brand; it's a community committed to making a difference. We pledge 1% of our sales to environmental preservation, ensuring that with every purchase, you're contributing to the health of our planet. Our commitment extends beyond the digital realm, embracing sustainable practices in our clothing line to minimize our environmental footprint.",
   },
 ]
+
+export default function Home() {
+  return (
+    <>
+      <MaxWidthWrapper>
+        <div className='py-20 mx-auto text-center flex flex-col items-center max-w-3xl'>
+          <h1 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl'>
+          Dive into our collection today and discover the possibilities that await. {' '}
+            <span className='text-blue-600'>
+              Clothing & Digital assets
+            </span>
+            .
+          </h1>
+          <p className='mt-6 text-lg max-w-prose text-muted-foreground'>
+          Welcome to the intersection of automotive passion and lifestyle expression — welcome to Ecxtatic.
+          </p>
+          <div className='flex flex-col sm:flex-row gap-4 mt-6'>
+            <Link
+              href='/products'
+              className={buttonVariants()}>
+              Browse Trending
+            </Link>
+            
+          </div>
+        </div>
+
+        <ProductReel
+          query={{ sort: 'desc', limit: 4 }}
+          href='/products?sort=recent'
+          title='Brand new'
+        />
+      </MaxWidthWrapper>
+
+      <section className='border-t border-gray-200 bg-gray-50'>
+        <MaxWidthWrapper className='py-20'>
+          <div className='grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-0'>
+            {perks.map((perk) => (
+              <div
+                key={perk.name}
+                className='text-center md:flex md:items-start md:text-left lg:block lg:text-center'>
+                <div className='md:flex-shrink-0 flex justify-center'>
+                  <div className='h-16 w-16 flex items-center justify-center rounded-full bg-blue-100 text-blue-900'>
+                    {<perk.Icon className='w-1/3 h-1/3' />}
+                  </div>
+                </div>
