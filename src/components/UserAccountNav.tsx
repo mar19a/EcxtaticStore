64aa463e8e4d02755420c3155bdf0417316c07b0
Mariano Majano Amaya <mariano@bu.edu>
2024-06-02 01:16:39 -0400

    Setting up signout functionality

---
+++ src/components/UserAccountNav.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import { User } from '@/payload-types'
+import { Button } from './ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+import Link from 'next/link'
+import { useAuth } from '@/hooks/use-auth'
+
+const UserAccountNav = ({ user }: { user: User }) => {
+  const { signOut } = useAuth()
+
+  return (
+
+  )
+}
+
+export default UserAccountNav
