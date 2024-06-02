a2ad788ad1514a51980c4c90f297439935778d9e
Mariano Majano Amaya <mariano@bu.edu>
2024-06-02 00:33:43 -0400

    Now the footer. We will minimize the /verify-email, /sign-up, and /sign-in path

---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { usePathname } from 'next/navigation'
+import MaxWidthWrapper from './MaxWidthWrapper'
+import { Icons } from './Icons'
+import Link from 'next/link'
+
+const Footer = () => {
+  const pathname = usePathname()
+  const pathsToMinimize = [
+    '/verify-email',
+    '/sign-up',
+    '/sign-in',
+  ]
+
+
+export default Footer
