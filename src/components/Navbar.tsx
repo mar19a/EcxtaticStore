cced85fbdcc8a0612331764143b432a2fabe3607
Mariano Majano Amaya <mariano@bu.edu>
2024-06-02 00:40:43 -0400

    Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import MaxWidthWrapper from './MaxWidthWrapper'
+import { Icons } from './Icons'
+import NavItems from './NavItems'
+import { buttonVariants } from './ui/button'
+import Cart from './Cart'
+import { getServerSideUser } from '@/lib/payload-utils'
+import { cookies } from 'next/headers'
+import UserAccountNav from './UserAccountNav'
+import MobileNav from './MobileNav'
+
+const Navbar = async () => {
+  const nextCookies = cookies()
+  const { user } = await getServerSideUser(nextCookies)
+
+export default Navbar
