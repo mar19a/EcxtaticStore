2f8c9f4d124c58f581b533ed26fdcba55268a6f3
Mariano Majano Amaya <mariano@bu.edu>
2024-06-02 00:56:40 -0400

    Showing all of the product listings

---
+++ src/components/ProductListing.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { Product } from '@/payload-types'
+import { useEffect, useState } from 'react'
+import { Skeleton } from './ui/skeleton'
+import Link from 'next/link'
+import { cn, formatPrice } from '@/lib/utils'
+import { PRODUCT_CATEGORIES } from '@/config'
+import ImageSlider from './ImageSlider'
+
+interface ProductListingProps {
+  product: Product | null
+  index: number
+}
+
+const ProductListing = ({
+  product,
+  index,
+}: ProductListingProps) => {
+  const [isVisible, setIsVisible] = useState<boolean>(false)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsVisible(true)
+    }, index * 75)
+
+    return () => clearTimeout(timer)
+  }, [index])
+
+  if (!product || !isVisible) return <ProductPlaceholder />
+
+  const label = PRODUCT_CATEGORIES.find(
+    ({ value }) => value === product.category
+  )?.label
+
+  const validUrls = product.images
+    .map(({ image }) =>
+      typeof image === 'string' ? image : image.url
+    )
+    .filter(Boolean) as string[]
+
+
+
+export default ProductListing
