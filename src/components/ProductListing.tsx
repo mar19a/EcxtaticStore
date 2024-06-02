6c5d593dc69ff7f6b9d92bbdb230b76deb27a530
Mariano Majano Amaya <mariano@bu.edu>
2024-06-02 00:57:14 -0400

    Layout will be product image, label, and price

--- src/components/ProductListing.tsx
+++ src/components/ProductListing.tsx
@@ -39,6 +39,31 @@ const ProductListing = ({
     )
     .filter(Boolean) as string[]
 
+  if (isVisible && product) {
+    return (
+      <Link
+        className={cn(
+          'invisible h-full w-full cursor-pointer group/main',
+          {
+            'visible animate-in fade-in-5': isVisible,
+          }
+        )}
+        href={`/product/${product.id}`}>
+        <div className='flex flex-col w-full'>
+          <ImageSlider urls={validUrls} />
 
-
+          <h3 className='mt-4 font-medium text-sm text-gray-700'>
+            {product.name}
+          </h3>
+          <p className='mt-1 text-sm text-gray-500'>
+            {label}
+          </p>
+          <p className='mt-1 font-medium text-sm text-gray-900'>
+            {formatPrice(product.price)}
+          </p>
+        </div>
+      </Link>
+    )
+  }
+}
 export default ProductListing
