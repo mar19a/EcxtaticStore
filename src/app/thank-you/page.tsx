a2dd662c41fff44df98db91ae2c89b916c565a5c
Mariano Majano Amaya <mariano@bu.edu>
2024-06-02 00:01:55 -0400

    Displaying order summary when the purchase is successful

---
+++ src/app/thank-you/page.tsx
@@ -0,0 +1,37 @@
+import { getServerSideUser } from '@/lib/payload-utils'
+import Image from 'next/image'
+import { cookies } from 'next/headers'
+import { getPayloadClient } from '@/get-payload'
+import { notFound, redirect } from 'next/navigation'
+import { Product, ProductFile, User } from '@/payload-types'
+import { PRODUCT_CATEGORIES } from '@/config'
+import { formatPrice } from '@/lib/utils'
+import Link from 'next/link'
+import PaymentStatus from '@/components/PaymentStatus'
+
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined
+  }
+}
+
+const ThankYouPage = async ({
+  searchParams,
+}: PageProps) => {
+  const orderId = searchParams.orderId
+  const nextCookies = cookies()
+
+  const { user } = await getServerSideUser(nextCookies)
+  const payload = await getPayloadClient()
+
+  const { docs: orders } = await payload.find({
+    collection: 'orders',
+    depth: 2,
+    where: {
+      id: {
+        equals: orderId,
+      },
+    },
+  })
+
+  export default ThankYouPage
