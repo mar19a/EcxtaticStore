0c04fd8a020b0ba3bd114615038ff17e047d9067
Mariano Majano Amaya <mariano@bu.edu>
2024-06-02 00:23:46 -0400

    Lastly, user can read or download their purchased products. While admins will have access to update or delete it

--- src/collections/ProductFile.ts
+++ src/collections/ProductFile.ts
@@ -62,3 +62,39 @@ const yourOwnAndPurchased: Access = async ({ req }) => {
     },
   }
 }
+
+export const ProductFiles: CollectionConfig = {
+  slug: 'product_files',
+  admin: {
+    hidden: ({ user }) => user.role !== 'admin',
+  },
+  hooks: {
+    beforeChange: [addUser],
+  },
+  access: {
+    read: yourOwnAndPurchased,
+    update: ({ req }) => req.user.role === 'admin',
+    delete: ({ req }) => req.user.role === 'admin',
+  },
+  upload: {
+    staticURL: '/product_files',
+    staticDir: 'product_files',
+    mimeTypes: [
+      'image/*',
+      'font/*',
+      'application/postscript',
+    ],
+  },
+  fields: [
+    {
+      name: 'user',
+      type: 'relationship',
+      relationTo: 'users',
+      admin: {
+        condition: () => false,
+      },
+      hasMany: false,
+      required: true,
+    },
+  ],
+}
