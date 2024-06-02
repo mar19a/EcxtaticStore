215824c7139bbb331e98553c7fb720b7d620ba60
Mariano Majano Amaya <mariano@bu.edu>
2024-06-02 00:41:57 -0400

    Aligning logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,4 +13,19 @@ const Navbar = async () => {
   const nextCookies = cookies()
   const { user } = await getServerSideUser(nextCookies)
 
+  return (
+    <div className='bg-white sticky z-50 top-0 inset-x-0 h-16'>
+      <header className='relative bg-white'>
+        <MaxWidthWrapper>
+          <div className='border-b border-gray-200'>
+            <div className='flex h-16 items-center'>
+              <MobileNav />
+
+              <div className='ml-4 flex lg:ml-0'>
+                <Link href='/'>
+                  <Icons.logo className='h-10 w-10' />
+                </Link>
+              </div>
+
+)
 export default Navbar
