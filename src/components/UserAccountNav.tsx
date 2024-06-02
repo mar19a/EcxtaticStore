f2d1d5811f5f06c587f4eb995caf985f56ba5306
Mariano Majano Amaya <mariano@bu.edu>
2024-06-02 01:17:31 -0400

    Fetching user email

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -27,6 +27,17 @@ const UserAccountNav = ({ user }: { user: User }) => {
         My account
       </Button>
     </DropdownMenuTrigger>
+
+    <DropdownMenuContent
+        className='bg-white w-60'
+        align='end'>
+        <div className='flex items-center justify-start gap-2 p-2'>
+          <div className='flex flex-col space-y-0.5 leading-none'>
+            <p className='font-medium text-sm text-black'>
+              {user.email}
+            </p>
+          </div>
+        </div>
   )
 }
 
