54061130eaf417b34f9e9c4a1d6415f073bcf5ed
Mariano Majano Amaya <mariano@bu.edu>
2024-06-02 00:03:32 -0400

    Making it soo the assests are downloadble on the thank you page

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -55,5 +55,45 @@ const ThankYouPage = async ({
     return total + product.price
   }, 0)
 
+  return (
+    <main className='relative lg:min-h-full'>
+      <div className='hidden lg:block h-80 overflow-hidden lg:absolute lg:h-full lg:w-1/2 lg:pr-4 xl:pr-12'>
+        <Image
+          fill
+          src='/checkout-thank-you.jpg'
+          className='h-full w-full object-cover object-center'
+          alt='thank you for your order'
+        />
+      </div>
+
+      <div>
+        <div className='mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8 lg:py-32 xl:gap-x-24'>
+          <div className='lg:col-start-2'>
+            <p className='text-sm font-medium text-blue-600'>
+              Order successful
+            </p>
+            <h1 className='mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl'>
+              Thanks for ordering
+            </h1>
+            {order._isPaid ? (
+              <p className='mt-2 text-base text-muted-foreground'>
+                Your order was processed and your assets are
+                available to download below. We&apos;ve sent
+                your receipt and order details to{' '}
+                {typeof order.user !== 'string' ? (
+                  <span className='font-medium text-gray-900'>
+                    {order.user.email}
+                  </span>
+                ) : null}
+                .
+              </p>
+            ) : (
+              <p className='mt-2 text-base text-muted-foreground'>
+                We appreciate your order, and we&apos;re
+                currently processing it. So hang tight and
+                we&apos;ll send you confirmation very soon!
+              </p>
+            )}
+
 
   export default ThankYouPage
