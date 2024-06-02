a6788c7cec1e11fe778471312726f32cea0faa47
Mariano Majano Amaya <mariano@bu.edu>
2024-06-02 00:25:50 -0400

    Roles successfully established and the access

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -24,4 +24,47 @@ export const Users: CollectionConfig = {
       },
     },
   },
- 
+  access: {
+    read: adminsAndUser,
+    create: () => true,
+    update: ({ req }) => req.user.role === 'admin',
+    delete: ({ req }) => req.user.role === 'admin',
+  },
+  admin: {
+    hidden: ({ user }) => user.role !== 'admin',
+    defaultColumns: ['id'],
+  },
+  fields: [
+    {
+      name: 'products',
+      label: 'Products',
+      admin: {
+        condition: () => false,
+      },
+      type: 'relationship',
+      relationTo: 'products',
+      hasMany: true,
+    },
+    {
+      name: 'product_files',
+      label: 'Product files',
+      admin: {
+        condition: () => false,
+      },
+      type: 'relationship',
+      relationTo: 'product_files',
+      hasMany: true,
+    },
+    {
+      name: 'role',
+      defaultValue: 'user',
+      required: true,
+
+      type: 'select',
+      options: [
+        { label: 'Admin', value: 'admin' },
+        { label: 'User', value: 'user' },
+      ],
+    },
+  ],
+}
