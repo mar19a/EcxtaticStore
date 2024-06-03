785bebb714d0e5a472d62cce5b74728d0639559c
Mariano Majano Amaya <mariano@bu.edu>
2024-06-02 20:10:02 -0400

    Correctly referencing editors picks path

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,13 @@
 export const PRODUCT_CATEGORIES = [
   {
+    label: 'Merch',
+    value: 'Merch' as const,
+    featured: [
+      {
+        name: 'Editor picks',
+        href: `/products?category=ui_kits`,
+        imageSrc: '/nav/ui-kits/mixed.jpg',
+      },
 
 
   },
