53d3ec632160e5fab3ca40ab1ba986fd55012e15
Mariano Majano Amaya <mariano@bu.edu>
2024-06-05 14:54:58 -0400

    Adding checkout picture

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -58,9 +58,8 @@ const ThankYouPage = async ({
   return (
     <main className='relative lg:min-h-full'>
       <div className='hidden lg:block h-80 overflow-hidden lg:absolute lg:h-full lg:w-1/2 lg:pr-4 xl:pr-12'>
-        <Image
-          fill
-          src='/checkout-thank-you.jpg'
+        <img
+          src='/checkout.png'
           className='h-full w-full object-cover object-center'
           alt='thank you for your order'
         />
